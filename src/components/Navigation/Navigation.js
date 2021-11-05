a7a0d604ea450214f1dcc9e2777d4cd200dfa1d2
PadraigF95 <44588037+PadraigF95@users.noreply.github.com>
2021-11-05 02:48:33 +0000

    Navigation bar

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Navigation.css'
+
+const Navigation = () => {
+    return (
+        <div className="Header-Container">
+            <div className="Navigation">
+                <nav className="Header">
+                <ul className="nav-links">
+                    <li><a href="#"> 00 Home</a></li>
+                    <li><a href="#">01 Destination</a></li>
+                    <li><a href="#">02 Crew</a></li>
+                    <li><a href="#">03 Technology</a></li>
+                </ul>
+                </nav>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Navigation
